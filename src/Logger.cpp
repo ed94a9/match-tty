f0691d1468bf239a917772ff56fcbc34578b0900
xizcheng <xizcheng@192.168.5.54>
2026-05-24 19:21:59 +0800

    Removed recursively printed title bar

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -3,7 +3,6 @@
 #include <quill/Frontend.h>
 #include <quill/Backend.h>
 #include <quill/sinks/FileSink.h>
-#include <quill/sinks/ConsoleSink.h>
 #include <quill/core/FrontendOptions.h>
 #include <quill/backend/SignalHandler.h>
 
@@ -17,9 +16,8 @@ void initLogger(std::string_view log_file) {
     quill::Backend::start<quill::FrontendOptions>(quill::BackendOptions{}, quill::SignalHandlerOptions{});
 
     auto file_sink = quill::Frontend::create_or_get_sink<quill::FileSink>(std::string(log_file));
-    auto stdout_sink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>("stdout");
 
-    g_logger = quill::Frontend::create_or_get_logger("match-tty", {file_sink, stdout_sink});
+    g_logger = quill::Frontend::create_or_get_logger("match-tty", {file_sink});
 }
 
 quill::Logger* logger() {
